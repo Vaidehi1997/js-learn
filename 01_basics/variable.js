da405e84155276b7ca5430f44eb20e4f8736b46c
Vaidehi1997 <40157753+Vaidehi1997@users.noreply.github.com>
2024-12-02 07:32:58 +0000

    variable learn

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let accountEmail = "demo@gmail.com"
+var accountcity = "surat";
+
+
+accountcity="kosamba"
+
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountcity ]);
+console.table([accountcity, accountEmail]);
