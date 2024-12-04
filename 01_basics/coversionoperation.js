0a5db3d1a0fbf6fa083422d9f59f40938c1e2e8e
Vaidehi1997 <40157753+Vaidehi1997@users.noreply.github.com>
2024-12-04 07:25:35 +0000

    conversion learn

---
+++ 01_basics/coversionoperation.js
@@ -0,0 +1,45 @@
+let score = true;
+
+//------ Number Conversion ----------
+let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+let valueNumber1 = Number(score);
+
+let isLoggedIn = undefined;
+
+//------ Boolean Conversion ----------
+let isLoggedInValue = Boolean(isLoggedIn);
+// console.log(typeof isLoggedInValue);
+// console.log(isLoggedInValue);
+
+
+//NaN = Not a number
+
+//------ String Conversion ----------
+
+let somenumber = 33
+
+let somenumberValue = String(somenumber);
+// console.log(somenumberValue);
+// console.log(typeof somenumberValue);
+
+
+// *********************** Operations ***********************
+ 
+let value = 3
+let negvalue = -3
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "vaidehi"
+
+console.log(str1 + " " + str2);
